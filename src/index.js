bcf810cb78c3e24b17542217b69141b8614122fb
haronchick <haron.www@mail.ru>
2018-02-23 00:32:28 +0300

    solution task quadratic equation | add file .gitignore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
+
   // your implementation
-}
+  let resultArray = [];
+  //  -20 * x^2 - 108797540 * x - 130011773690520
+
+  let reg = /(.\d+)\s.\s...\s([+-].\d+)\s\*\s.\s([+-].\d+)/;
+  let match = equation.match(reg);
+  let a = +match[1].replace(' ','');
+  let b = +match[2].replace(' ','');
+  let c = +match[3].replace(' ','');
+  let d = b**2 - (4 * a * c);
+
+  if (d > 0) {
+      let x1 = (-b + Math.sqrt(d)) / (2 * a);
+      let x2 = (-b - Math.sqrt(d)) / (2 * a);
+      resultArray.push(Math.round(x1),Math.round(x2));
+  }
+
+    return resultArray.sort((a,b) => a > b ? 1 : -1);
+};
